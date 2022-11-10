960628cafbd81a0b39985c8a8701e30672bbf7ea
modhu <modhugamba@gmail.com>
2022-11-10 09:43:25 -0500

    updated new_script

---
+++ new_script.py
@@ -0,0 +1,14 @@
+import os
+import shutil
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.cm import get_cmap
+
+x=7
+y=5
+
+z=x+y
+
+print('the z value is updated in branch new_dir',z)
+
+print('the z value is updated in branch new_dir',z)
